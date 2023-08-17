128d63c28a76a9905870033b19c3b36a761bd391
IgnaDev <arcortoon@gmail.com>
2023-08-17 15:52:43 -0300

    Terminado el login y register

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,58 @@
+import {Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Grid, TextField, Button, Link } from "@mui/material";
+import AuthLayout from "../layout/AuthLayout";
+
+
 export default function Login() {
-  return (
-    <div>Login</div>
+  return (  
+      <AuthLayout titulo="Login">
+        <form action="">
+          <Grid container
+          >
+            <Grid item xs={12} sx={{mt:2}}
+            >
+              <TextField
+                id=""
+                label="Correo"
+                type="email"
+                placeholder="Correo electronico"
+                fullWidth
+              />
+
+            </Grid>
+            <Grid item xs={12} sx={{mt:2}}
+            >
+              <TextField
+              id=""
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+              />
+            </Grid>
+            <Grid container spacing={2} sx={{mb:2, mt:1}}>
+              <Grid item xs={12} sm={8} >
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Button variant="contained" fullWidth>
+                  <Google/>
+                </Button>
+              </Grid>
+            </Grid>
+              <Grid container direction="row" justifyContent="end">
+                <Link 
+                component={RouterLink}
+                color="inherit"
+                to="/auth/register"
+                > Create una cuenta</Link>
+              </Grid>
+          </Grid>
+        </form>
+      </AuthLayout>
+
   )
 }
--- src/auth/pages/Registe.jsx
+++ src/auth/pages/Registe.jsx
@@ -1,6 +1,63 @@
+import {Link as RouterLink } from "react-router-dom";
+
+import { Grid, TextField, Button, Link } from "@mui/material";
+import AuthLayout from "../layout/AuthLayout";
+
 
 export default function Registe() {
   return (
-    <div>Registe</div>
+    <AuthLayout titulo="Registro">
+    <form action="">
+      <Grid container
+      >
+        <Grid item xs={12} sx={{mt:2}}
+        >
+          <TextField
+          id=""
+          label="Nombre"
+          type="text"
+          placeholder="Juan Perez"
+          fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sx={{mt:2}}
+        >
+          <TextField
+            id=""
+            label="Correo"
+            type="email"
+            placeholder="Correo electronico"
+            fullWidth
+          />
+
+        </Grid>
+        <Grid item xs={12} sx={{mt:2}}
+        >
+          <TextField
+          id=""
+          label="Contraseña"
+          type="password"
+          placeholder="Contraseña"
+          fullWidth
+          />
+        </Grid>
+        <Grid container spacing={2} sx={{mb:2, mt:1}}>
+          <Grid item xs={12} sm={12} >
+            <Button variant="contained" fullWidth>
+              Registrarme
+            </Button>
+          </Grid>
+
+        </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Link 
+            component={RouterLink}
+            color="inherit"
+            to="/auth/login"
+            > ¿Ya tienes cuenta?</Link>
+          </Grid>
+      </Grid>
+    </form>
+  </AuthLayout>
   )
 }
